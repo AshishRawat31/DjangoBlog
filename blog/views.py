774fa26440a0aaaaf712fbfdbf8a60631a715eae
Ashishrawat31 <Ashishrawat.dtu31@gmail.com>
2017-03-09 10:36:17 +0530

    additional files and changes for Heroku

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,15 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
